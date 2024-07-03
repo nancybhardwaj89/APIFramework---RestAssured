b04e63abe04b120472619ba39259e2c9a58f4997
Nancy <er.nancybhardwaj@gmail.com>
2024-07-03 17:39:59 +0530

    Updated Code

--- src/test/java/api/gorest/tests/CreateUser.java
+++ src/test/java/api/gorest/tests/CreateUser.java
@@ -12,7 +12,6 @@ public class CreateUser extends BaseTest {
 	@Test
 	public void createUserTest() {
 		UserRequest usereq = new UserRequest("nancy", StringUtils.getRandomEmails(), "female", "active");
-		// RestClient restclient1 = new RestClient();
 		Integer userID = restclient.post("/public/v2/users", "json", usereq, true).then().log().all().assertThat()
 				.statusCode(201).extract().path("id");
 		System.out.println("User ID is:" + userID);
